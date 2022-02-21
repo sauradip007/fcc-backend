09d76fd34d3b4ad90737be5ce42e9fc585f69106
Sauradip Sengupta <sauradip.sengupta@gmail.com>
2022-02-21 14:18:24 +0530

    boiler-plate dropped

---
+++ myApp.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+const port = 3001;
+console.log("Hello World");
+
+// app.get("/", (req,res)=>{
+//     res.sendFile(__dirname + '/views/index.html')
+// })
+
+app.get("/json", (req, res) => {
+  const obj = { message: "Hello json" };
+  res.json(obj);
+});
+app.use("/public", express.static("public"));
+app.listen(port, () => {
+  console.log(`Hello World`);
+});
+
+module.exports = app;
